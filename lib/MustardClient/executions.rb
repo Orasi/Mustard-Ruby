952967cfe8f7309f92130ddd87f5f1e353101b4d
Matt Watson <matt.watson@orasi.com>
2016-09-27 16:25:32 -0400

    0.1.6

--- lib/MustardClient/executions.rb
+++ lib/MustardClient/executions.rb
@@ -2,6 +2,67 @@ require 'MustardClient/client'
 module MustardClient
   class ExecutionsClient < Client
 
+
+    def environment_detail execution_id, environment_id
+
+      command = {}
+      command[:method] = :get
+      command[:route] = @mustard_url + "/executions/#{execution_id}/environment/#{environment_id}"
+      command[:headers] = {'User-Token' => @user_token}
+
+      execute(command)
+
+    end
+
+
+    def testcase_detail execution_id, testcase_id
+
+      command = {}
+      command[:method] = :get
+      command[:route] = @mustard_url + "/executions/#{execution_id}/testcases/#{testcase_id}"
+      command[:headers] = {'User-Token' => @user_token}
+
+      execute(command)
+
+    end
+
+
+    def testcase_summary execution_id
+
+      command = {}
+      command[:method] = :get
+      command[:route] = @mustard_url + "/executions/#{execution_id}/testcase_summary"
+      command[:headers] = {'User-Token' => @user_token}
+
+      execute(command)
+
+    end
+
+
+    def environment_summary execution_id
+
+      command = {}
+      command[:method] = :get
+      command[:route] = @mustard_url + "/executions/#{execution_id}/environment_summary"
+      command[:headers] = {'User-Token' => @user_token}
+
+      execute(command)
+
+    end
+
+
+    def testcase_status execution_id
+
+      command = {}
+      command[:method] = :get
+      command[:route] = @mustard_url + "/executions/#{execution_id}/testcase_status"
+      command[:headers] = {'User-Token' => @user_token}
+
+      execute(command)
+
+    end
+
+
     def find execution_id
 
       command = {}
@@ -13,6 +74,7 @@ module MustardClient
 
     end
 
+
     def close execution_id
 
       command = {}
@@ -23,6 +85,7 @@ module MustardClient
       execute(command)
 
     end
+
 
     def delete execution_id
 
